9fe7895f5252139be4d9525ef908fece34c134bb
Arasu <arasu088@gmail.com>
2021-11-26 08:38:17 +0530

    bundle installed capistrano configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-server '169.254.232.162', user: 'ubuntu', roles: %w{web app db}
+server '13.58.8.97', user: 'ubuntu', roles: %w{web app db}
 set :ssh_options, { 
     forward_agent: true, 
       auth_methods: %w[publickey],
