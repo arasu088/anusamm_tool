6aad0f8a104a8a5bfa6d566afd37af1416b4dac5
Arasu <arasu088@gmail.com>
2021-11-26 21:15:04 +0530

    changed commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,6 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Default value for keep_releases is 5
  set :keep_releases, 5
-
+ set :linked_files, %w{config/master.key}
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
